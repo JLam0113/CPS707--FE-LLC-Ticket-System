7e95b23242e9a878cd2e8a51fd536836a9b31e00
Christopher Seow <christopherseow456@gmail.com>
2020-02-11 13:40:40 -0500

    Write to file - create
    Added functionality to create new user, writes to accounts.txt
    
    Fixed the accounts.txt path so its read locally

--- src/com/cps/fe/user/User.java
+++ src/com/cps/fe/user/User.java
@@ -1,26 +1,37 @@
 package com.cps.fe.user;
 
+import java.awt.Component;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+import com.cps.fe.console.Console;
 
 public class User {
 	String username;
 	String userType;
 	int credit;
 
+
 	public User(String username) {
 		this.username = username;
 		try {
-		File file = new File("C:\\Users\\Jessye\\eclipse-workspace\\CPS707- FE LLC Ticket System\\src\\resources\\accounts.txt");
+		
+		//Get the local path for accounts.txt
+		java.net.URL url = User.class.getClassLoader().getResource("resources/accounts.txt");
+			
+		File file = new File(url.getPath());
 		Scanner sc = new Scanner(file);
 		while (sc.hasNextLine()) {
 			String temp = sc.nextLine();
 			String[] temp2 = temp.split(" ");
 			if(temp2[0].equals(this.username))
 				this.userType = temp2[1];
-				this.credit = Integer.parseInt(temp2[2]);
-				sc.close();
+				this.credit = (int) Double.parseDouble(temp2[2]);
 			}
 		sc.close();
 		}
@@ -31,7 +42,11 @@ public class User {
 	
 	public boolean exists() {
 		try {
-		File file = new File("C:\\Users\\Jessye\\eclipse-workspace\\CPS707- FE LLC Ticket System\\src\\resources\\accounts.txt");
+		
+		//Get the local path for accounts.txt
+		java.net.URL url = User.class.getClassLoader().getResource("resources/accounts.txt");
+			
+		File file = new File(url.getPath());	
 		Scanner sc = new Scanner(file);
 		while (sc.hasNextLine()) {
 			String temp = sc.nextLine();
@@ -50,7 +65,11 @@ public class User {
 	
 	public void updateCredit(int credit) {
 		try {
-			File file = new File("C:\\Users\\Jessye\\eclipse-workspace\\CPS707- FE LLC Ticket System\\src\\resources\\accounts.txt");
+			
+			//Get the local path for accounts.txt
+			java.net.URL url = User.class.getClassLoader().getResource("resources/accounts.txt");
+				
+			File file = new File(url.getPath());			
 			Scanner sc = new Scanner(file);
 			while (sc.hasNextLine()) {
 				String temp = sc.nextLine();
@@ -66,9 +85,46 @@ public class User {
 				}
 	}
 	
-	public void createAccount(String username, String type) {
+	public void createAccount(String username, String type){
 		if (this.userType.equals("AA")) {
-			//Write to file with new account info
+			try 
+			{
+				//Write to file with new account info
+				//Get the local path for accounts.txt
+				java.net.URL url = User.class.getClassLoader().getResource("resources/accounts.txt");
+					
+				File file = new File(url.getPath());
+				Scanner sc = new Scanner(file);
+				
+				while (sc.hasNextLine()) 
+				{
+					String temp = sc.nextLine();
+					String[] temp2 = temp.split(" ");
+					if(temp2[0].equals(username))
+					{
+						//An account with this name already exists
+						System.out.println("Invalid username (username already taken). Session ended.");
+						System.exit(0);
+					}
+				}
+				sc.close();
+				
+				//User name is not taken.
+				FileWriter fr;
+				try {
+					fr = new FileWriter(file,true);
+					fr.write("\n" + username + " " + type + " 0.00");
+					fr.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+			catch (FileNotFoundException e) 
+			{
+				 System.out.println(e);
+			}	
 		}
 		else 
 			System.out.println("You are not authorized to do that. please enter a command");
