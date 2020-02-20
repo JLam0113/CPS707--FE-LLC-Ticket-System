6e5ed876d111b411d85f8b4741e8cd42ef521485
mtsao-slc <michaeltsao001@gmail.com>
2020-02-20 00:37:49 -0500

    file record IO implemented and DTF

--- src/com/cps/fe/console/Console.java
+++ src/com/cps/fe/console/Console.java
@@ -20,7 +20,7 @@ public class Console {
 
 		while(true) {
 		User user1 = login(sc);
-		tickets = new Tickets(user1);
+		tickets = new Tickets(user1, sc);
 		postLogin(user1, sc);
 		logout(user1);
 		}
@@ -68,7 +68,7 @@ public class Console {
 				float price = Float.parseFloat(sc.nextLine());
 				System.out.println("Please enter the amount of tickets:");
 				int numOfTickets = Integer.parseInt(sc.nextLine());
-				tickets.sell(eventTitle, price, numOfTickets); // TODO: remove username and use inited one
+				tickets.sell(eventTitle, price, numOfTickets);
 			}
 			if(next.equals("refund")) {
 				
